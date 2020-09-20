54a3e7bf73c552583dfe88332467e89c58ef0635
yashu159357 <yashu159357@gmail.com>
2020-09-20 14:37:10 +0530

    Updates

---
+++ precache-manifest.953898bf10e8f450f02b986e8379cb62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8d14f29c22eb1e3cc191bae3406c80",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "50e419b7482ca077bc34",
+    "url": "/clock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bdb6713ad626a615777e",
+    "url": "/clock/static/js/2.be0c0d2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.be0c0d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e419b7482ca077bc34",
+    "url": "/clock/static/js/main.cfc7a557.chunk.js"
+  },
+  {
+    "revision": "9fbb0c64d7d7588fb297",
+    "url": "/clock/static/js/runtime-main.bf40dc10.js"
+  },
+  {
+    "revision": "27fa3619ef47d565608b38c13d7bd68f",
+    "url": "/clock/static/media/clo.27fa3619.bmp"
+  }
+]);
